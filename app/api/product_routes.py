9b3a178b89607b4a9c3fa9b182f7362048cfb7ae
Jake Ye <jayyeucla@gmail.com>
2022-10-14 17:53:35 -0700

    add get all product route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -4,16 +4,15 @@ from app.models import Product
 
 product_routes = Blueprint('products', __name__)
 
-#get all product 
-@user_routes.route('/')
-@login_required
-def users():
-    users = User.query.all()
-    return {'users': [user.to_dict() for user in users]}
+#get all product
+@product_routes.route('/')
 
+def products():
+    products = Product.query.all()
+    return {'products': [product.to_dict() for product in products]}
 
-@user_routes.route('/<int:id>')
-@login_required
-def user(id):
-    user = User.query.get(id)
-    return user.to_dict()
+#get one product
+@product_routes.route('/<int:id>')
+def product(id):
+    product = Product.query.get(id)
+    return product.to_dict()
