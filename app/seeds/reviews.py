a194146dd94f186584fdf42f09650aae459feb46
linyangofmay <linyangofmay@gmail.com>
2022-10-14 20:58:38 -0700

    donewiththeseed

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -1,244 +1,281 @@
 from app.models import db, Review
 
 def seed_reviews():
-review01 = Review(
+    review01 = Review(
     productId = 2,
     userId = 1,
     stars = 5,
     comment = 'Super cute and sizing is just right. I ordered a size up for a loose fit that I can wear with leggings. Obsessed with this design!!',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review02 = Review(
+    )
+    review02 = Review(
     productId = 15,
     userId = 1,
     stars = 5,
     comment = 'I ordered the wrong item so I contacted the seller and they were more than happy to accommodate. So I ended up getting the dragon ball with blue light and my 10 year old daughter is thrilled with it.',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review03 = Review(
+    )
+    review03 = Review(
     productId = 15,
     userId = 2,
     stars = 5,
     comment = "It feels like I'm in the anime when I hold the pokeball. The color and presentation are outstanding.",
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review04 = Review(
+    )
+    review04 = Review(
     productId = 6,
     userId = 2,
     stars = 5,
     comment = "I love the fact that it comes in various sizes for us to download! Super cute as well!",
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review05 = Review(
+    )
+    review05 = Review(
     productId = 6,
     userId = 1,
     stars = 5,
     comment = "So simple but so beautiful! It just looks good no matter where I place it :)",
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review06 = Review(
+    )
+    review06 = Review(
     productId = 3,
     userId = 1,
     stars = 5,
     comment = "Printed beautifully and very crisp",
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review07 = Review(
+    )
+    review07 = Review(
     productId = 4,
     userId = 1,
     stars = 5,
     comment = "Great quality print. Highly recommend",
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review08 = Review(
+    )
+    review08 = Review(
     productId = 5,
     userId = 1,
     stars = 5,
     comment = "I'm obsessed with how my painting turned out!!! Couldn't even wait for my fiance's birthday to give it to him!",
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review09 = Review(
+    )
+    review09 = Review(
     productId = 7,
     userId = 2,
     stars = 5,
     comment = "I am in love with this!! I had it printed and laminated. And just hung it in my gorls bedroom! It us exactly what I'd envisioned!",
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review10 = Review(
+    )
+    review10 = Review(
     productId = 9,
     userId = 2,
     stars = 5,
     comment = "The cutest prints for my son’s beach themed room! I love that there are different sizes of the prints included in the digital file so you can pick and choose print sizes. Love them, they’re so cute! Great quality too!",
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review11 = Review(
+    )
+    review11 = Review(
     productId = 16,
     userId = 1,
     stars = 4,
     comment = 'Super cute and sizing is just right. ',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review12 = Review(
+    )
+    review12 = Review(
     productId = 17,
     userId = 1,
     stars = 4,
     comment = 'The box this comes in is 4 yard by 5 kilometer and weights 11 pound!',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review13 = Review(
+    )
+    review13 = Review(
     productId = 18,
     userId = 1,
     stars = 3,
     comment = 'My neighbor Honora has one of these. She works as a reporter and she says it looks enormous.',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review14 = Review(
+    )
+    review14 = Review(
     productId = 19,
     userId = 1,
     stars = 5,
     comment = 'My co-worker Ali has one of these. He says it looks towering.',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review15 = Review(
+    )
+    review15 = Review(
     productId = 20,
     userId = 1,
     stars = 4,
     comment = 'My co-worker Kazuo has one of these. He says it looks transparent.',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review16 = Review(
+    )
+    review16 = Review(
     productId = 21,
     userId = 1,
     stars = 5,
-    comment = 'My co-worker Merwin has one of these. He says it looks bubbly.
-    this Product is top-notch.',
+    comment = 'My co-worker Merwin has one of these. He says it looks bubbly.',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review17 = Review(
+    )
+    review17 = Review(
     productId = 22,
     userId = 1,
     stars = 4,
     comment = 'My neighbor Lonnie has one of these. She works as a hobbit and she says it looks microscopic',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review18 = Review(
+    )
+    review18 = Review(
     productId = 23,
     userId = 1,
     stars = 5,
     comment = 'My neighbor Alida has one of these. She works as a gambler and she says it looks spotless.',
     createdAt= "2021-11-19 20:39:36",
     updatedAt= "2021-11-19 20:39:36",
-)
-review19 = Review(
+    )
+    review19 = Review(
     productId = 24,
     userId = 1,
     stars = 4,
-    comment = 'Lovely product. Will buy again in the future. Thank you!'
+    comment = 'Lovely product. Will buy again in the future. Thank you!',
     createdAt= "2021-11-19 20:39:36",
-    updatedAt= "2021-11-19 20:39:36",
-)
-review20 = Review(
+    updatedAt= "2021-11-19 20:39:36"
+    )
+    review20 = Review(
     productId = 25,
     userId = 1,
     stars = 4,
-    comment = 'Nice usage. Will buy again the next time I have more allowance.'
+    comment = 'Nice usage. Will buy again the next time I have more allowance.',
     createdAt= "2021-11-19 20:39:36",
-    updatedAt= "2021-11-19 20:39:36",
-)
-review21 = Review(
+    updatedAt= "2021-11-19 20:39:36"
+    )
+    review21 = Review(
     productId = 31,
     userId = 20,
     stars = 5,
-    comment = "Beautiful yarn. It's sooo soft. I received all the colors I asked for and a sweet little gift as well."
+    comment = "Beautiful yarn. It's sooo soft. I received all the colors I asked for and a sweet little gift as well.",
     createdAt= "2022-10-19 20:39:36",
-    updatedAt= "2021-10-19 20:39:36",
-)
-review22 = Review(
+    updatedAt= "2021-10-19 20:39:36"
+    )
+    review22 = Review(
     productId = 32,
     userId = 21,
     stars = 5,
-    comment = 'Ordering this yarn was a breeze.'
+    comment = 'Ordering this yarn was a breeze.',
     createdAt= "2022-10-19 20:39:36",
     updatedAt= "2021-10-19 20:39:36",
-)
-review23 = Review(
+    )
+    review23 = Review(
     productId = 33,
     userId = 21,
     stars = 5,
-    comment = 'I am very happy with this purchase. The yarn is exactly as described and the color is identical to the one shown online.'
+    comment = 'I am very happy with this purchase. The yarn is exactly as described and the color is identical to the one shown online.',
     createdAt= "2022-10-19 20:39:36",
     updatedAt= "2021-10-19 20:39:36",
-)
-review24 = Review(
+    )
+    review24 = Review(
     productId = 34,
     userId = 21,
     stars = 5,
-    comment = 'Ordering this yarn was a breeze.'
+    comment = 'Ordering this yarn was a breeze.',
     createdAt= "2022-10-19 20:39:36",
     updatedAt= "2021-10-19 20:39:36",
-)
-review25 = Review(
+    )
+    review25 = Review(
     productId = 35,
     userId = 21,
     stars = 5,
-    comment = 'Softest and most beautiful yarn ever! Not only super light and cloud soft.'
+    comment = 'Softest and most beautiful yarn ever! Not only super light and cloud soft.',
     createdAt= "2022-10-19 20:39:36",
     updatedAt= "2021-10-19 20:39:36",
-)
-review26 = Review(
+    )
+    review26 = Review(
     productId = 36,
     userId = 23,
     stars = 5,
-    comment = 'This watercolor is stunning!'
+    comment = 'This watercolor is stunning!',
     createdAt= "2022-10-19 20:39:36",
     updatedAt= "2021-10-19 20:39:36",
-)
-review27 = Review(
+    )
+    review27 = Review(
     productId = 37,
     userId = 24,
     stars = 5,
-    comment = 'I just adore this painting, I couldn't be any happier!'
+    comment = 'I just adore this painting, I couldn't be any happier!',
     createdAt= "2022-10-19 20:39:36",
     updatedAt= "2021-10-19 20:39:36",
-)
-review28 = Review(
+    )
+    review28 = Review(
     productId = 38,
     userId = 25,
     stars = 5,
-    comment = 'I love this quilt art watercolor print by William Gardner of Gardnerwatercolors!'
+    comment = 'I love this quilt art watercolor print by William Gardner of Gardnerwatercolors!',
     createdAt= "2022-10-19 20:39:36",
     updatedAt= "2021-10-19 20:39:36",
-)
-review29 = Review(
+    )
+    review29 = Review(
     productId = 39,
     userId = 27,
     stars = 5,
-    comment = 'It’s beautiful and exceeded my expectations! The colors are fabulous and I can’t wait to frame it and hang it on my wall. Thank you William!!!'
+    comment = 'It’s beautiful and exceeded my expectations! The colors are fabulous and I can’t wait to frame it and hang it on my wall. Thank you William!!!',
     createdAt= "2022-10-19 20:39:36",
     updatedAt= "2021-10-19 20:39:36",
-)
-review30 = Review(
+    )
+    review30 = Review(
     productId = 40,
     userId = 29,
     stars = 5,
-    comment = 'The color was very vibrant and the print was high quality.'
+    comment = 'The color was very vibrant and the print was high quality.',
     createdAt= "2022-10-19 20:39:36",
     updatedAt= "2021-10-19 20:39:36",
-)
+    )
+    db.session.add(review01)
+    db.session.add(review02)
+    db.session.add(review03)
+    db.session.add(review04)
+    db.session.add(review05)
+    db.session.add(review06)
+    db.session.add(review07)
+    db.session.add(review08)
+    db.session.add(review09)
+    db.session.add(review10)
+    db.session.add(review11)
+    db.session.add(review12)
+    db.session.add(review13)
+    db.session.add(review14)
+    db.session.add(review15)
+    db.session.add(review16)
+    db.session.add(review17)
+    db.session.add(review18)
+    db.session.add(review19)
+    db.session.add(review20)
+    db.session.add(review21)
+    db.session.add(review22)
+    db.session.add(review23)
+    db.session.add(review24)
+    db.session.add(review25)
+    db.session.add(review26)
+    db.session.add(review27)
+    db.session.add(review28)
+    db.session.add(review29)
+    db.session.add(review30)
+
+
+    db.session.commit()
+
+
+def undo_reviews():
+    db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
+    db.session.commit()
