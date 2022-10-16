f6a6152defde47c57ff1ea27254a1cde6e6349b5
Kevin Kim <kebonkim@gmail.com>
2022-10-16 14:22:18 -0700

    still working on productdetail page

--- react-app/src/components/Products/ProductDetailPage.js
+++ react-app/src/components/Products/ProductDetailPage.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkGetOneProduct } from "../../store/product";
+import './ProductDetailPage.css'
 
 
 function ProductDetailPage() {
@@ -21,19 +22,55 @@ function ProductDetailPage() {
     },[dispatch, id])
 
     return (
-        <div>
-            <h1>Product DETAIL PAGE</h1>
-        <div>
-            <div>{product.name}</div>
-            <div>{product.price}</div>
-            <div>{product.description}</div>
-            <div>{product.highlight}</div>
-            <div>{product.category}</div>
-            <img src={product.previewImage} alt='product' width={250} height={250}></img>
+      <div>
+        <div className="productcontainer">
+          <div>
+            <img
+              className="productdetailimage"
+              src={product?.previewImage}
+              alt="product"
+            ></img>
+          </div>
+          <div className="productdetail">
+            <div>USERNAME WILL GO HERE</div>
+            <div className="productdetailname">{product?.name}</div>
+            <div className="productdetailprice">{`$${product?.price}`}</div>
+            <div>QUANTITY WILL GO HERE</div>
+            <div style={{ padding: "5px" }}>
+              <button className="Buynow_button">Buy it now</button>
+            </div>
+            <div style={{ padding: "5px" }}>
+              <button className="Addtocart_button">Add to cart</button>
+            </div>
+            <div className="descriptiondetail_cart">
+              <i className="fa-solid fa-cart-shopping fa-2xl"></i>
+              <div className="descriptiondetail_text">
+                Other people want this. Over 10 people have this in their carts
+                right now.
+              </div>
+            </div>
+            <div className="descriptiondetail_medal">
+              <i class="fa-solid fa-medal fa-2xl"></i>
+              <div className="descriptiondetail_text">
+                Star Seller. This seller consistently earned 5-star reviews,
+                shipped on time, and replied quickly to any messages they
+                received.
+              </div>
+            </div>
+            <div className="descriptiondetail_cart">
+              <i class="fa-solid fa-van-shuttle fa-2xl"></i>
+              <div className="descriptiondetail_text">
+                <span className="hooray_text">Hooray!&nbsp;</span>This item
+                ships free to the US.
+              </div>
+            </div>
+            <div>{product?.description}</div>
+            <div>{product?.highlight}</div>
+            <div>{product?.category}</div>
+          </div>
         </div>
-        </div>
-        
-    )
+      </div>
+    );
 }
 
 export default ProductDetailPage
