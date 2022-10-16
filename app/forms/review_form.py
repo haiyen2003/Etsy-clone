02031b48186dc5f935bd09167ca31638d443df6b
Jake Ye <jayyeucla@gmail.com>
2022-10-16 14:02:58 -0700

    update review form

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, IntegerField
-from wtforms.validators import DataRequired, ValidationError, URL
+from wtforms.validators import DataRequired, ValidationError, URL, NumberRange
 from app.models import Review
 
 
@@ -19,5 +19,5 @@ def imageURL_validation(form, field):
 
 class ReviewForm(FlaskForm):
     review = StringField('review', validators=[DataRequired(), review_validation ])
-    stars = IntegerField('stars', validators=[DataRequired()])
+    stars = IntegerField('stars', validators=[DataRequired(), NumberRange(min=1, max=5, message="Stars must be between 1 to 5")])
     reviewImg = StringField('reviewimg', validators=[URL(require_tld=True, message="Please enter a valid URL"), imageURL_validation])
