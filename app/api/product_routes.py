8a885e5091518607a5be8ad9c7d916943b1d907e
Jake Ye <jayyeucla@gmail.com>
2022-10-15 21:50:04 -0700

    testing edit product route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -54,9 +54,24 @@ def add_product():
 def update_product():
     form = ProductForm()
     form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        edit_product = Product.query.get(id)
-        if edit_product.userId != current_user.id:
-            return {"errors" : "You don't have the right to edit the product"}, 403
+    edit_product = Product.query.get(id)
 
-        
+    if edit_product is None:
+        return {"errors" : "You don't have the right to edit the product"}, 403
+    if edit_product.userId != current_user.id:
+        return {"errors" : "Product couldn't be found"}, 404
+
+    if form.validate_on_submit():
+
+        edit_product.name = form.data['name'],
+        edit_product.description = form.data['description'],
+        edit_product.price = form.data['price'],
+        edit_product.category = form.data['category'],
+        edit_product.highlight = form.data['highlight'],
+        edit_product.previewImage = form.data['previewImage'],
+        edit_product.updatedAt = now
+
+        db.session.add(edit_product)
+        db.session.commit()
+        return edit_product.to_dict()
+    return {"errors" : validation_errors_to_error_messages(form.errors)}, 400
