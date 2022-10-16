a89f4b7c428ad79df0409533249c725d0f74c506
Jake Ye <jayyeucla@gmail.com>
2022-10-15 22:36:10 -0700

    working on edit product route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -51,27 +51,29 @@ def add_product():
 #update product
 @product_routes.route('/<int:id>/edit', methods=["PUT"])
 @login_required
-def update_product():
+def update_product(id):
+    print(id)
     form = ProductForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     edit_product = Product.query.get(id)
-
+    print(edit_product)
     if edit_product is None:
-        return {"errors" : "You don't have the right to edit the product"}, 403
-    if edit_product.userId != current_user.id:
         return {"errors" : "Product couldn't be found"}, 404
+    if edit_product.userId != current_user.id:
+        return {"errors" : "You don't have the right to edit the product"}, 403
 
     if form.validate_on_submit():
-
+        edit_product = Product.query.get(id)
         edit_product.name = form.data['name'],
         edit_product.description = form.data['description'],
         edit_product.price = form.data['price'],
         edit_product.category = form.data['category'],
         edit_product.highlight = form.data['highlight'],
         edit_product.previewImage = form.data['previewImage'],
+        # edit_product.userId = current_user.id,
+        # edit_product.createdAt = now,
         edit_product.updatedAt = now
-
-        db.session.add(edit_product)
+        # db.session.add(edit_product)
         db.session.commit()
         return edit_product.to_dict()
     return {"errors" : validation_errors_to_error_messages(form.errors)}, 400
