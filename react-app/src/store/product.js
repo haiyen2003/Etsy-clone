c0e30a660b4848b1ee21b07364dad44ede9bd7e0
linyangofmay <linyangofmay@gmail.com>
2022-10-17 10:15:24 -0700

    change backstick to single quotation

--- react-app/src/store/product.js
+++ react-app/src/store/product.js
@@ -71,7 +71,7 @@ export const thunkCreateProduct = (payload) => async dispatch => {
 }
 
 export const thunkGetAllProduct = () => async dispatch => {
-    const response = await fetch(`/api/products`, {
+    const response = await fetch('/api/products', {
       method: "GET",
       header: { "Content-Type": "application/json" },
     });
