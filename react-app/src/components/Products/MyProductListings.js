9824581981faa4c57aa5896c0e2430fbca8e85d5
Kevin Kim <kebonkim@gmail.com>
2022-10-17 22:36:44 -0700

    myproductlistings css

--- react-app/src/components/Products/MyProductListings.js
+++ react-app/src/components/Products/MyProductListings.js
@@ -6,13 +6,15 @@ import { NavLink } from "react-router-dom";
 import ProductUpdate from "./ProductUpdate";
 import ProductDelete from "./ProductDelete";
 
+import './MyProductListing.css'
+
 
 function MyProductListings() {
     const dispatch = useDispatch()
     const history = useHistory()
 
     const user = useSelector(state => state.session.user)
-    // console.log(user)
+    console.log(user)
 
     useEffect(() => {
         dispatch(thunkGetCurrentProduct())
@@ -25,27 +27,32 @@ function MyProductListings() {
     if (!user) history.push('/')
 
   return (
-        <div>
-            <div>My Shop</div>
-            <div>
-                {currentProductArr.map((product, i) => (
-                    <div key={i}>
-                        <div>
-                            <NavLink to={`/products/${product?.i}`}>
-                                <img alt="product" src={product?.previewImage} width={200}>
-                                </img>
-                            </NavLink>
-                        </div>
-                        <div>{product.name}</div>
-                        <div>
-                            <ProductUpdate product={product}/>
-                            <ProductDelete product={product}/>
-                        </div>
-                    </div>
-                ))}
+    <div className="my_product_listing_div">
+      <h1>{`${user.firstName}'s Shop`}</h1>
+      <div className="my_product_listing_box">
+        {currentProductArr.map((product, i) => (
+          <div  key={i}>
+            <div className="my_product_listing_innerbox">
+            <div className="my_product_listing_nav">
+              <NavLink to={`/products/${product?.id}`}>
+                <img
+                  alt="product"
+                  src={product?.previewImage}
+                  className="my_product_listing_img"
+                ></img>
+              </NavLink>
+            <div className="my_product_listing_name">{product.name}</div>
             </div>
-        </div>
-  )
+            <div>
+              <ProductUpdate product={product} />
+              <ProductDelete product={product} />
+            </div>
+          </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default MyProductListings;
