19470f3e657961293caf72c8db25a59132905c04
Jake Ye <jayyeucla@gmail.com>
2022-10-19 11:32:08 -0700

    update product form

--- app/forms/product_form.py
+++ app/forms/product_form.py
@@ -38,13 +38,13 @@ def imageURL_validation(form, field):
 
 Categories_Choices = ["Home & Living", "Art & Collectibles", "Clothing & Shoes", "Jewelry & Accessories", "Wedding & Party", "Personalized Gifts"]
 
-Hightlight_Choices = []
+Hightlight_Choices = ["Materials", "Handmade", "Made to Order"]
 
 class ProductForm(FlaskForm):
     name = StringField("Product Name", validators= [DataRequired(), name_validation])
     description = TextAreaField("Product Description", validators= [DataRequired(), description_validation])
     price = DecimalField("Price", validators=[DataRequired(), price_validation], places=2, rounding = ROUND_HALF_UP)
     category = SelectField("Category", choices = Categories_Choices, validators=[DataRequired()])
-    highlight = StringField("Highlights", validators=[DataRequired(), highlights_validation])
+    highlight = SelectField("Highlight", choices = Hightlight_Choices, validators=[DataRequired(), highlights_validation])
     previewImage = StringField("Image URL", validators= [DataRequired(), imageURL_validation])
     submit = SubmitField("Submit")
